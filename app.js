481ccb70cc912a30964d9fe58120e93cce472fea
evojroan <roan6903@gmail.com>
2024-03-25 07:29:23 +0800

    第二次上傳，總共有前置作業與資料驗證功能

--- app.js
+++ app.js
@@ -1,36 +1,113 @@
+//綠界電子發票技術文件： https://developers.ecpay.com.tw/?p=7809
 const crypto = require("crypto");
 const axios = require("axios");
+const fs = require("fs");
 
-//改以下就好
-const MerchantID = "2000132";
+////////////////////////改以下參數即可////////////////////////
+//一、選擇帳號，是否為測試環境
+const MerchantID = "2000132"; //必填
 const HashKey = "ejCk326UnaZWKisg"; //2000132
 const HashIV = "q9jcZX8Ib9LM8wYk"; //2000132
+let isStage = true;
+const PlatformID = "";
 
-const query = 0; //0：驗證統一編號；1：驗證手機條碼；2：驗證捐贈碼
-const UnifiedBusinessNo = "97025978"; //要查詢的統一編號，僅限數字
-const Barcode = ""; //要查詢的手機條碼。格式應為8碼字元，第1碼為『/』; 其餘7碼則由數字【0-9】、大寫英文【A-Z】與特殊符號【+】【-】【.】這39個字元組成;
-const LoveCode = ""; //要查詢的捐贈碼
-let isStage = true; //true：測試環境；false：正式環境
+//二、選擇 何種 API 作業
+const action = 2; // 0：查詢財政部配號結果； 1：字軌與配號設定； 2：設定字軌號碼狀態； 3：查詢字軌； 4：驗證統一編號；5：驗證手機條碼；6：驗證捐贈碼
 
-const PlatformID = ""; //這個參數是專為與綠界簽約的指定平台商所設計，只有在申請開通後才能使用。一般廠商，請在介接時將此參數欄位保留為空。
+//三、根據不同的 action 填寫以下作業的參數
+//action = 0
+const InvoiceYear0 = ""; //必填
 
-//以下不用改
+//action = 1：字軌與配號設定(InvoiceCategory 已設定好，本處不用寫)
+const InvoiceTerm = ""; //必填
+const InvoiceYear1 = ""; //必填
+const InvType = "07"; //必填
+const InvoiceHeader = ""; //必填
+const InvoiceStart = ""; //必填
+const InvoiceEnd = ""; //必填
+
+//action = 2：設定字軌號碼狀態
+const TrackID = ""; //必填
+const InvoiceStatus = ""; //必填
+
+//action = 3：查詢字軌(InvoiceCategory 已設定好，本處不用寫)
+const InvoiceYear3 = ""; //必填
+const InvoiceTerm3 = ""; //必填
+const UseStatus = "0"; //必填
+const InvType3 = "07";
+const InvoiceHeader3 = "";
+
+//action = 4：統一編號驗證
+const UnifiedBusinessNo = "97025978"; //必填
+
+//action = 5：手機條碼驗證
+const Barcode = ""; //必填
+
+//action = 6：捐贈碼驗證
+const LoveCode = ""; //必填
+
+////////////////////////以下不用改////////////////////////
 const stage = isStage ? "-stage" : "";
 const APIURL =
-  query === 0
+  action === 0
+    ? `https://einvoice${stage}.ecpay.com.tw/B2CInvoice/GetGovInvoiceWordSetting`
+    : action === 1
+    ? `https://einvoice${stage}.ecpay.com.tw/B2CInvoice/AddInvoiceWordSetting`
+    : action === 2
+    ? `https://einvoice${stage}.ecpay.com.tw/B2CInvoice/UpdateInvoiceWordStatus`
+    : action === 3
+    ? `https://einvoice${stage}.ecpay.com.tw/B2CInvoice/GetInvoiceWordSetting`
+    : action === 4
     ? `https://einvoice${stage}.ecpay.com.tw/B2CInvoice/GetCompanyNameByTaxID`
-    : query === 1
+    : action === 5
     ? `https://einvoice${stage}.ecpay.com.tw/B2CInvoice/CheckBarcode`
-    : query === 2
+    : action === 6
     ? `https://einvoice${stage}.ecpay.com.tw/B2CInvoice/CheckLoveCode`
     : "";
 const algorithm = "aes-128-cbc";
-const DataArray = {
-  "MerchantID": MerchantID,
-  "UnifiedBusinessNo": UnifiedBusinessNo,
-  "Barcode": Barcode,
-  "LoveCode": LoveCode
+let DataArray = {
+  "MerchantID": MerchantID
 };
+if (action === 0) {
+  DataArray = {
+    ...DataArray,
+    "InvoiceYear": InvoiceYear0
+  };
+} else if (action === 1) {
+  DataArray = {
+    ...DataArray,
+    "InvoiceTerm": InvoiceTerm,
+    "InvoiceYear": InvoiceYear1,
+    "InvType": InvType,
+    "InvoiceCategory": 1,
+    "InvoiceHeader": InvoiceHeader,
+    "InvoiceStart": InvoiceStart,
+    "InvoiceEnd": InvoiceEnd
+  };
+} else if (action === 2) {
+  DataArray = {
+    ...DataArray,
+    "TrackID": TrackID,
+    "InvoiceStatus": InvoiceStatus
+  };
+} else if (action === 3) {
+  DataArray = {
+    ...DataArray,
+    "InvoiceYear": InvoiceYear3,
+    "InvoiceTerm": InvoiceTerm3,
+    "UseStatus": UseStatus,
+    "InvoiceCategory": 1,
+    "InvType": InvType3,
+    "InvoiceHeader": InvoiceHeader3
+  };
+} else if (action === 4) {
+  DataArray = {...DataArray, "UnifiedBusinessNo": UnifiedBusinessNo};
+} else if (action === 5) {
+  DataArray = {...DataArray, "Barcode": Barcode};
+} else if (action === 6) {
+  DataArray = {...DataArray, "LoveCode": LoveCode};
+}
+
 let URLEncoded = encodeURIComponent(JSON.stringify(DataArray));
 
 const cipher = crypto.createCipheriv(algorithm, HashKey, HashIV);
@@ -46,20 +123,39 @@ const parameters = {
   "Data": EncryptedData
 };
 
-axios
-  .post(APIURL, parameters)
-  .then(response => {
-    const ToBeDecrypted = response.data.Data;
+(async () => {
+  const open = await import("open");
 
-    const decipher = crypto.createDecipheriv(algorithm, HashKey, HashIV);
-    let DecryptedData = decipher.update(ToBeDecrypted, "base64", "utf8");
-    DecryptedData += decipher.final("utf8");
+  axios
+    .post(APIURL, parameters)
+    .then(async response => {
+      const ToBeDecrypted = response.data.Data;
 
-    console.log(
-      "綠界系統回傳：\n\n",
-      JSON.stringify(response.data, null, 2),
-      "\n\nData 解密後：\n\n",
-      decodeURIComponent(DecryptedData)
-    );
-  })
-  .catch(error => console.log(error));
+      const decipher = crypto.createDecipheriv(algorithm, HashKey, HashIV);
+      let DecryptedData = decipher.update(ToBeDecrypted, "base64", "utf8");
+      DecryptedData += decipher.final("utf8");
+
+      const htmlContent = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>顯示文本</title>
+</head>
+<body>
+<h1>綠界回傳：</h1>
+    <p>${JSON.stringify(response.data, null, 2)}</p>
+<h1>Data 解密後：</h1>
+<p>${decodeURIComponent(DecryptedData)}</p>
+</body>
+</html>
+`;
+      const filePath = "output.html";
+      fs.writeFile(filePath, htmlContent, err => {
+        if (err) throw err;
+
+        console.log("HTML檔案已創建");
+        open.default(filePath).catch(err => console.error(err));
+      });
+    })
+    .catch(error => console.log(error));
+})();
